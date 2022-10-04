f48ee367662f617e3cf697b6ac3a2e8674f68d7a
AhmedKidwai <ahmed.kidwai@boldcommerce.com>
2022-10-04 17:49:39 -0500

    array.go

--- Go-Tutorial/array.go
+++ Go-Tutorial/array.go
@@ -14,4 +14,40 @@ func main() {
 
 	myVal := arr1[0]
 	fmt.Println(myVal)
+	fmt.Println("The length of the array is: ", len(arr2))
+
+	// Let's loop an array with an iterator
+	for i := 0; i < len(arr2); i++ {
+		fmt.Println(arr2[i])
+	}
+
+	// Let's loop an array with a range
+	for i, v := range arr2 {
+		fmt.Printf("%d : %d\n", i, v)
+	}
+
+	fmt.Println("Multi Dimenonal Array")
+	arr3 := [2][2]int{
+		{1, 4},
+		{3, 2},
+	}
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 2; j++ {
+			fmt.Println(arr3[i][j])
+		}
+	}
+
+	// Slice
+
+	aStr1 := "abcd"
+	rArr := []rune(aStr1)
+
+	for _, v := range rArr {
+		fmt.Println("Rune Array: ", v)
+	}
+
+	byteArr := []byte{'a', 'b', 'c', 'd'}
+	bStr := string(byteArr[:])
+	fmt.Println(bStr)
+
 }
